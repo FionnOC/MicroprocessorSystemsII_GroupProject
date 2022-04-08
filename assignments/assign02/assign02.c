1d3344cc2a3a63d927691eb032e9a91dfc4720e8
olearyd3 <olearyd3@tcd.ie>
2022-04-09 00:04:16 +0100

    Added Morse letters, rising and falling edge gpio and made LED blue when on the start screen

--- assignments/assign02/assign02.c
+++ assignments/assign02/assign02.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "pico/stdlib.h"
 #include "hardware/gpio.h"
+#include "hardware/pio.h"
+#include "hardware/clocks.h"
+#include "assign02.pio.h"
+
+#define IS_RGBW true        // Will use RGBW format
+#define NUM_PIXELS 1        // There is 1 WS2812 device in the chain
+#define WS2812_PIN 28       // The GPIO pin that the WS2812 connected to
+
+char letters[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '?'};
 
 // Must declare the main assembly entry point before use.
 void main_asm();
@@ -34,11 +44,43 @@ void asm_gpio_set_irq_fall(uint pin) {
 void asm_gpio_set_irq_rise(uint pin) {
     gpio_set_irq_enabled(pin, GPIO_IRQ_EDGE_RISE, true);
 }
+
+// wrapper function to push 32-bit RGB colour value out to LED serially
+static inline void put_pixel(uint32_t pixel_grb) {
+    pio_sm_put_blocking(pio0, 0, pixel_grb << 8u);
+}
+
+// generate a 32-bit int composit RGB value by combining individual 8-bit params
+static inline uint32_t urgb_u32(uint8_t r, uint8_t g, uint8_t b) {
+    return  ((uint32_t) (r) << 8)  |
+            ((uint32_t) (g) << 16) |
+            (uint32_t) (b);
+}
+
+// function to display which level has been chosen
+void levelChooser(int index) {
+    char inp = letters[index];
+    // level 1
+    if(inp == '1') {
+        printf("Level 1 starting...\n\n");
+    }
+    // level 2
+    else if(inp == '2') {
+        printf("Level 2 starting...\n\n");
+    }
+    else {
+        printf("Incorrect morse sequence entered... Please retry!\n\n");
+    }
+}
 /*
  * Main entry point for the code - simply calls the main assembly function.
  */
 int main() {
     stdio_init_all();// Initialise all basic IO
+
+    gpio_set_irq_enabled(21, GPIO_IRQ_EDGE_FALL, true);
+    gpio_set_irq_enabled(21, GPIO_IRQ_EDGE_RISE, true);
+
     printf("+---------------------------------------------+\n");
     printf("|          Assignment 2 Lab Group 20          |\n");
     printf("+---------------------------------------------+\n");
@@ -61,8 +103,16 @@ int main() {
     printf("|   Enter Sequence on GP21 to choose Level    |\n");
     printf("|                                             |\n");
     printf("|   ""-----""  - Level #1 - CHARS (EASY)          |\n");
-    printf("|   "".----""  - Level #2 - WORDS (HARD)          |\n");
+    printf("|   "".----""  - Level #2 - CHARS (HARD)          |\n");
     printf("+---------------------------------------------+\n");
+
+    // Initialise the PIO interface with the WS2812 code
+    PIO pio = pio0;
+    uint offset = pio_add_program(pio, &ws2812_program);
+    ws2812_program_init(pio, 0, offset, WS2812_PIN, 800000, IS_RGBW);
+    // Set the color to blue at half intensity
+    put_pixel(urgb_u32(0x00, 0x00, 0x7F));
+
     main_asm();
     return(0);
 }
