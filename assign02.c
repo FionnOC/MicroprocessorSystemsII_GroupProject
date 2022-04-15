7bb77a3f8accf753b580b71ab4ec47477de253fd
Fionn O Connor <oconnof9@tcd.ie>
2022-04-15 10:54:04 +0100

    Started on Doxygen Commenting and fixed error inside print_input_result

--- assign02.c
+++ assign02.c
@@ -17,13 +17,13 @@ char morse_letters[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
                         'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 
                         'Y', 'Z', '1', '2', '3', '4', '5', '6', 
                         '7', '8', '9', '0', '?'};
-int morse_encoder[] = {501, 5100, 51010, 5100, 50, 50010, 5110, // A-G
+int morse_encoder[] = {501, 51000, 51010, 5100, 50, 50010, 5110, // A-G
                         50000, 500, 50111, 5101, 50100, 511, 510, // H-N
                         5111, 50110, 51101, 5010, 5000, 51, 5001, // O-U
                         50001, 5011, 51001, 51011, 51100, 501111, // V-1
                         500111, 500011, 500001, 500000, 510000, // 2-6
                         511000, 511100, 511110, 511111}; // 7-0
-char* morsetable[] = {
+char* morsetable[] = {  
     // Letters A-Z (indices 0-25)
     ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....",
     "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.",
@@ -34,48 +34,100 @@ char* morsetable[] = {
     "-...", "--...", "---..", "----.", "-----"
 };
 
+
 int lives = 3;
 int int_maker = 5;
 int count;
 int level1_finished = 0;
 int finished_game = 0;
 int level = 0;
+int alarm_flag = 0;
+int total_lives_lost = 0;
+int total_lives_gained = 0;
 
 
-// Must declare the main assembly entry point before use.
+
+/**
+ * @brief Must declare the main assembly entry point before use.
+ * 
+ */
 void main_asm();
 
-// Initialise a GPIO pin – see SDK for detail on gpio_init()
+/**
+ * @brief  Initialise a GPIO pin – see SDK for detail on gpio_init()
+ * 
+ * @param pin 
+ */
 void asm_gpio_init(uint pin) {
     gpio_init(pin);
 }
 
-// Set direction of a GPIO pin – see SDK for detail on gpio_set_dir()
+/**
+ * @brief  Set direction of a GPIO pin – see SDK for detail on gpio_set_dir()
+ * 
+ * @param pin 
+ * @param out 
+ */
 void asm_gpio_set_dir(uint pin, bool out) {
     gpio_set_dir(pin, out);
 }
 
-// Get the value of a GPIO pin – see SDK for detail on gpio_get()
+/**
+ * @brief Get the value of a GPIO pin – see SDK for detail on gpio_get()
+ * 
+ * @param pin 
+ * @return true 
+ * @return false 
+ */
 bool asm_gpio_get(uint pin) {
     return gpio_get(pin);
 }
 
-// Set the value of a GPIO pin – see SDK for detail on gpio_put()
+/**
+ * @brief Set the value of a GPIO pin – see SDK for detail on gpio_put()
+ * 
+ * @param pin 
+ * @param value 
+ */
 void asm_gpio_put(uint pin, bool value) {
     gpio_put(pin, value);
 }
 
-// Enable falling-edge interrupt – see SDK for detail on gpio_set_irq_enabled()
+/**
+ * @brief Enable falling-edge interrupt – see SDK for detail on gpio_set_irq_enabled()
+ * 
+ * @param pin 
+ */
 void asm_gpio_set_irq(uint pin) {
     gpio_set_irq_enabled(pin, GPIO_IRQ_EDGE_FALL|GPIO_IRQ_EDGE_RISE, true);
 }
 
-// wrapper function to push 32-bit RGB colour value out to LED serially
+/**
+ * @brief wrapper function to push 32-bit RGB colour value out to LED serially
+ * 
+ * @param pixel_grb 
+ */
 static inline void put_pixel(uint32_t pixel_grb) {
     pio_sm_put_blocking(pio0, 0, pixel_grb << 8u);
 }
 
-// generate a 32-bit int composit RGB value by combining individual 8-bit params
+/**
+ * @brief generate a 32-bit int composit RGB value by combining individual 8-bit params
+ * 
+ * @param r 
+ * @param g 
+ * @param b 
+ * @return uint32_t 
+ */
+
+/**
+ * @brief 
+ * 
+ * @param r 
+ * @param g 
+ * @param b 
+ * @return uint32_t 
+ */
 static inline uint32_t urgb_u32(uint8_t r, uint8_t g, uint8_t b) {
     return  ((uint32_t) (r) << 8)  |
             ((uint32_t) (g) << 16) |
@@ -83,24 +135,56 @@ static inline uint32_t urgb_u32(uint8_t r, uint8_t g, uint8_t b) {
 }
 
 absolute_time_t start_time;
+
+/**
+ * @brief fetches and stores the current time
+ * 
+ */
 void start_timer(){
     start_time = get_absolute_time();
 }
 
+/**
+ * @brief calculates the difference between the rising edge and the falling edge to differentiate between a dot and dash
+ * 
+ * @return int 
+ */
 int end_timer(){
     int time_diff = (int) absolute_time_diff_us(start_time, get_absolute_time());
-    printf("%d\n", time_diff);
-    if (time_diff > 250000) {
+    return time_diff;
+}
+
+/**
+ * @brief sets the alarm flag; tells the rest of the .c code that the alarm in the .S file has been fired
+ * 
+ */
+void alarm_call(){
+    alarm_flag = 1;
+}
+
+/**
+ * @brief takes in a dot and dash from .S and updates current input
+ * 
+ * @param bit_arm 
+ */
+void morse_parser(int bit_arm){
+
+    if (bit_arm == 1) {
         int_maker = int_maker * 10;
         int_maker++;
     }
     else {
-        int_maker = 10*int_maker;
+        int_maker = 10 * int_maker;
     }
-    //watchdog_update();
-    return time_diff;
+
+    watchdog_update();
+
 }
 
+/**
+ * @brief print the welcome screen 
+ * 
+ */
 void welcomeScreen() {
     printf("+---------------------------------------------+\n");
     printf("|          Assignment 2 Lab Group 20          |\n");
@@ -125,29 +209,32 @@ void welcomeScreen() {
     printf("|                                             |\n");
     printf("|   "".----""  - Level #1 - CHARS (EASY)          |\n");
     printf("|   ""..---""  - Level #2 - CHARS (HARD)          |\n");
-    printf("|   ""...--""  - Level #3 - WORDS (EASY)          |\n");
-    printf("|   ""....-""  - Level #4 - WORDS (HARD)          |\n");
     printf("+---------------------------------------------+\n");
 }
 
-// function to display which level has been chosen
+
+/**
+ * @brief function to display which level has been chosen
+ * 
+ * @return int 
+ */
 int levelChooser() {
     printf("Level code: \n");
-    while (int_maker <= 501111) {
+    while (int_maker <= 501111 && alarm_flag != 1) {
         if (int_maker == 501111) {
-            printf("%i", int_maker);
+            
             return 1;
         }
         else if (int_maker == 500111) {
-            printf("%i", int_maker);
+            
             return 2;
         }
         else if (int_maker == 500011) {
-            printf("%i", int_maker);
+            
             return 3;
         }
         else if (int_maker == 500001) {
-            printf("%i", int_maker);
+
             return 4;
         }
     }
@@ -155,6 +242,11 @@ int levelChooser() {
     return 100;
 }
 
+/**
+ * @brief changes colour of the LED depending on the life count
+ * 
+ * @param lives 
+ */
 void life_indicator (int lives) {
     if (lives == 3) {
         put_pixel(urgb_u32(0x00, 0x2F, 0x00)); // green
@@ -171,6 +263,163 @@ void life_indicator (int lives) {
     else put_pixel(urgb_u32(0x00, 0x00, 0x2F)); // blue
 }
 
+/**
+ * @brief Prints the Alphanumeric Character equivalent of the morse input 
+ * 
+ */
+void print_input_result(){
+
+    switch(int_maker)
+    {
+        case 501:
+            printf("A");
+            break;
+
+        case 51000:
+            printf("B");
+            break;
+
+        case 51010:
+            printf("C");
+            break;
+
+        case 5100:
+            printf("D");
+            break;
+
+        case 50:
+            printf("E");
+            break;
+
+        case 50010:
+            printf("F");
+            break;
+
+        case 5110:
+            printf("G");
+            break;
+
+        case 50000:
+            printf("H");
+            break;
+
+        case 500:
+            printf("I");
+            break;
+
+        case 50111:
+            printf("J");
+            break;
+
+        case 5101:
+            printf("K");
+            break;
+
+        case 50100:
+            printf("L");
+            break;
+
+        case 511:
+            printf("M");
+            break;
+
+        case 510:
+            printf("N");
+            break;
+
+        case 5111:
+            printf("O");
+            break;
+
+        case 50110:
+            printf("P");
+            break;
+
+        case 51101:
+            printf("Q");
+            break;
+
+        case 5010:
+            printf("R");
+            break;
+
+        case 5000:
+            printf("S");
+            break;
+
+        case 51:
+            printf("T");
+            break;
+
+        case 5001:
+            printf("U");
+            break;
+
+        case 50001:
+            printf("V");
+            break;
+
+        case 5011:
+            printf("W");
+            break;
+
+        case 51001:
+            printf("X");
+            break;
+
+        case 51011:
+            printf("Y");
+            break;
+
+        case 51100:
+            printf("Z");
+            break;
+
+        case 511111:
+            printf("0");
+            break;
+
+        case 501111:
+            printf("1");
+            break;
+
+        case 500111:
+            printf("2");
+            break;
+
+        case 500011:
+            printf("3");
+            break;
+
+        case 500001:
+            printf("4");
+            break;
+
+        case 500000:
+            printf("5");
+            break;
+
+        case 510000:
+            printf("6");
+            break;
+
+        case 511000:
+            printf("7");
+            break;
+
+        case 511100:
+            printf("8");
+            break;
+        // operator doesn't match any case constant 
+        default:
+            printf("?");
+    }
+    
+
+}
+
+
+
 // function play the game based on what level is chosen
 
 void play() {
@@ -179,12 +428,16 @@ void play() {
     if(level == 1 && level1_finished != 1) {
         int_maker = 5;
         // need to update the morse values to have the letters and binary equivs in separate arrays
-        printf("Enter %c in Morse Code (Hint: %s)\n", morse_letters[value], morsetable[value] );
+        printf("Enter %c in Morse Code (Hint: %s)\n", morse_letters[value], morsetable[value]);
+        
 
-        while(int_maker <= morse_encoder[value] && count != 5) {
+        while(int_maker <= morse_encoder[value] && count != 5 && alarm_flag != 1) {
+            
+            sleep_us(100); // hard fault if not here
             if(int_maker == morse_encoder[value]) {
-                printf("That is correct! Good job!\n");
+                printf("\nYou enterd the correct sequence %c\n", morse_letters[value]);
                 count++;
+                total_lives_gained++;
                 if(count == 5){
                 level1_finished = 1;
                 }
@@ -194,8 +447,11 @@ void play() {
                 return;
             }            
         }
-        printf("That is incorrect :(\n");
+        printf("\nThe sequence ");
+        print_input_result(); 
+        printf(" you entered did not match %c\n",morse_letters[value]);
         count = 0;
+        total_lives_lost++;
         lives--;
         return;
     }
@@ -213,12 +469,15 @@ void play() {
 
       
         // need to update the morse values to have the letters and binary equivs in separate arrays
-        printf("Enter %c in Morse Code\n", morse_letters[value], morsetable[value] );
+        printf("Enter %c in Morse Code (Hint: %s)\n", morse_letters[value], morsetable[value] );
 
-        while(int_maker <= morse_encoder[value] && count != 5) {
+        while(int_maker <= morse_encoder[value] && count != 5 && alarm_flag != 1) {
+
+            sleep_us(100); // hard fault if no delay
             if(int_maker == morse_encoder[value]) {
-                printf("That is correct! Good job!\n");
+                printf("\nYou entered the correct sequence %c\n", morse_letters[value]);
                 count++;
+                total_lives_gained++;
                 if(count == 5){
                     finished_game = 1;
                 }
@@ -228,11 +487,15 @@ void play() {
                 return;
             }            
         }
-        printf("That is incorrect :(\n");
+        printf("\nThe sequence ");
+        print_input_result(); 
+        printf(" you entered did not match %c\n",morse_letters[value]);
+        total_lives_lost++;
         count = 0;
         lives--;
         return;
     }
+    
 }
 
 // function to start the game
@@ -247,13 +510,13 @@ void start_game() {
         play();
         // set the LED
         life_indicator(lives);
-        printf("\n%i\n", count);
-    }
-
-    if(lives == 0){
-        printf("Ran out of Lives!");
+        printf("Current Streak : %i\n", count);
+        alarm_flag = 0;
+       
     }
 }
+
+
  
  
 /*
@@ -265,72 +528,83 @@ int main() {
     srand(time(NULL));
     stdio_init_all();// Initialise all basic IO
 
-/*
     if (watchdog_caused_reboot()) {
             printf("Rebooted by Watchdog!\n");
             return 0;
         } else {
             printf("Clean boot\n");
         }
-*/
+
     watchdog_enable(8000000, 1);
-    
-    if(watchdog_caused_reboot()) {
-        printf("Rebooted by Watchdog!\n");
-    }
+
 
     // display the welcome screen
-    while(1) {
+    while(1){
 
-        lives = 3;
-        int_maker = 5;
-        count = 0;
-        level1_finished = 0;
-        finished_game = 0;
-        level = 0;
+    lives = 3;
+    int_maker = 5;
+    count = 0;
+    level1_finished = 0;
+    finished_game = 0;
+    level = 0;
+    total_lives_lost = 0;
+    total_lives_gained = 0;
 
-        main_asm();
-        // display the welcome screen
-        welcomeScreen();
+    main_asm();
+    welcomeScreen();
 
-        // Initialise the PIO interface with the WS2812 code
-        PIO pio = pio0;
-        uint offset = pio_add_program(pio, &ws2812_program);
-        ws2812_program_init(pio, 0, offset, WS2812_PIN, 800000, IS_RGBW);
-        put_pixel(urgb_u32(0x00, 0x00, 0x2F)); // Set the colour to blue
+    // Initialise the PIO interface with the WS2812 code
+    PIO pio = pio0;
+    uint offset = pio_add_program(pio, &ws2812_program);
+    ws2812_program_init(pio, 0, offset, WS2812_PIN, 800000, IS_RGBW);
+    put_pixel(urgb_u32(0x00, 0x00, 0x2F)); // Set the colour to blue
 
-        // choosing a level
-    
-        while(level == 0) {
+    //main_asm();
 
-            while ((level < 1 || level > 4) && level != 100) {
-                level = levelChooser();
-            }
+    // choosing a level
+  
+    while(level == 0) {
 
-            if(level != 100) {
-                printf("\nYou have selected Level %d.\n\n", level);
-            }
-
-            else {
-                printf("|   Enter Sequence on GP21 to choose Level    |\n");
-                printf("|                                             |\n");
-                printf("|   "".----""  - Level #1 - CHARS (EASY)          |\n");
-                printf("|   ""..---""  - Level #2 - CHARS (HARD)          |\n");
-                printf("|   ""...--""  - Level #3 - WORDS (EASY)          |\n");
-                printf("|   ""....-""  - Level #4 - WORDS (HARD)          |\n");
-                level = 0;
-                int_maker = 5;
-            }
-            
+        while ((level < 1 || level > 4) && level != 100) {
+            level = levelChooser();
         }
 
-        start_game();
-        if(lives == 0) {
-            printf("GAME OVER!!! Better luck next time!\n\n");
+        if(level != 100) {
+            printf("\nYou have selected Level %d.\n\n", level);
         }
-        if(finished_game == 1){
-            printf("Congratulations you are a master at ARM\n");
+
+        else {
+            printf("|   Enter Sequence on GP21 to choose Level    |\n");
+            printf("|                                             |\n");
+            printf("|   "".----""  - Level #1 - CHARS (EASY)          |\n");
+            printf("|   ""..---""  - Level #2 - CHARS (HARD)          |\n");
+
+            level = 0;
+            int_maker = 5;
+            alarm_flag = 0;
         }
+        
     }
-    return 0;
+
+
+    start_game();
+    if(lives == 0) {
+        printf("GAME OVER!!! Better luck next time!\n");
+    }
+
+    if(finished_game == 1){
+        printf("Congratulations you are a master at ARM\n");
+        }
+    
+    printf("+---------------------------+\n");
+    printf("| Total Lives Gained :  %-4i|\n", total_lives_gained);
+    printf("| Total Lives Lost :    %-4i|\n", total_lives_lost);
+    printf("| Total Attempts Made : %-4i|\n", total_lives_gained + total_lives_lost);
+    printf("| Sucess Rate :         %-4i|\n", 100*(total_lives_gained)/(total_lives_gained + total_lives_lost));
+    printf("| Level Reached :       %i   |\n", level);
+    printf("+---------------------------+\n");
+
+    sleep_ms(2);
+
+    }
 }
